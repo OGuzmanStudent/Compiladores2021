7d0733da2e544d3c150711307743551084273dbd
acardiawolfmanpop <acardiawolfmanpop@pop-os.localdomain>
2021-08-31 16:30:49 -0500

    Inicializacion de la practica 2

---
+++ Practica2/regextoafn.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+class ThompsonConstruction {
+public:
+    const string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    const char epsylon = 'E';
+    const char operator_kleene = '*';
+    const char operator_union = '|';
+    const char operator_open_parenthesis = '(';
+    const char operator_close_parenthesis = ')';
+    string regex;
+    ThompsonConstruction(string r):regex(s){};
+    void process():
+      
+};
+int main () {
+    
+    cin<<
+    ThompsonConstruction t;
+    cout<<t.alphabet;
+}
