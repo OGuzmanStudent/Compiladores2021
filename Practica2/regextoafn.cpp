9aa5ff7bd5e06bb637231f5bb3322da855a58bfb
acardiawolfmanpop <acardiawolfmanpop@pop-os.localdomain>
2021-09-03 20:00:43 -0500

    avance

--- Practica2/regextoafn.cpp
+++ Practica2/regextoafn.cpp
@@ -1,26 +1,20 @@
 #include <iostream>
+#include <list>
 #include "regextoafn.h"
 using namespace std;
-class TransitionPair {
+class AutomatonGroup {
 public:
-    char simbol;
-    TransitionPair* toState;
-
-    TransitionPair (char s) : simbol(s)
+    char operatorSimbol;
+    int operatorWeight;
+    int startParenthesis;
+    int endParenthesis;
+    //Ya que la concatenacion se produce con dos simbolos del alfabeto
+    //seguidos, considero que se trata de un 'operador intermedio' o implicito,
+    //almacenare este indice como un double para utilizar los indices enteros
+    //para hacer referencia a los operandos
+    double operatorIndex;
+    AutomatonGroup()
     {
-    };
-};
-class Af {
-public:
-    bool end;
-    int tableSize;
-    TransitionPair* transitionsTable;
-    Af(bool e, int ts, TransitionPair* tt):transitionsTable(tt), end(e), tableSize(ts) {};
-    TransitionPair* getNextState(char c) {
-        for (int i=0; i<tableSize; i++) {
-            if(transitionsTable[i].simbol == c) return transitionsTable[i].toState;
-        }
-        return NULL;
     };
 };
 
@@ -34,14 +28,17 @@ const char operator_open_parenthesis = '(';
 const char operator_close_parenthesis = ')';
 bool ThompsonConstruction::preprocess() {
     int matchParethesis = 0;
+    list<AutomatonGroup> operaciones;
     //< O P E R A T O R  ~ P R E C E D E N C E >
-    //->Grouping ()
-    //->Single-character-ERE duplication * + ? {m,n}
-    //->Concatenation
-    //->Alternation | n
+    //->Grouping () val =4
+    //->Single-character-ERE duplication * + ? {m,n} val=3
+    //->Concatenation val = 2
+    //->Alternation | ni val= 1
+    int index = 0;
     for (char symbol : regex) {
         switch(symbol) {
         case '(':
+//            operaciones.push_back()
             matchParethesis++;
             break;
         case ')':
@@ -52,8 +49,11 @@ bool ThompsonConstruction::preprocess() {
         case '|':
             break;
         default:
+            //Este caso seria la concatenacion ya que es lo que se representa con
+            //dos miembros del alfabeto consecutivos
             break;
         }
+        index++;
     }
     bool isValidExpression = matchParethesis == 0;
     return isValidExpression;
