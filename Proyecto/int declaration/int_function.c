f037b0b93f1095852b831cb365fffc47233cae1a
acardiawolfmanpop <oguzmanp1200@alumno.ipn.mx>
2021-12-08 17:09:18 -0600

    compilaciones varias

---
+++ Proyecto/int declaration/int_function.c
@@ -0,0 +1,8 @@
+int unaFuncion(int x) { return x + 123; }
+int main() {
+  int o;
+  int resultado;
+  o = 1;
+  resultado = unaFuncion(o);
+  return o;
+}
---
+++ Proyecto/int declaration/intxyz.c
@@ -0,0 +1,9 @@
+int main(){
+  int x;
+  int y;
+  int z;
+  x=1;
+  y=2;
+  z=3;
+  x=x+y+z;
+}
